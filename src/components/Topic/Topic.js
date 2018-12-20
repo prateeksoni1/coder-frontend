e3f1c1b56fae7168e7959f7df12c0b2a736116b9
Proforty7 <proforty7@gmail.com>
2018-12-20 19:45:40 +0530

    Added general topic
    Signed-off-by: Proforty7 <proforty7@gmail.com>

--- src/components/Topic/Topic.js
+++ src/components/Topic/Topic.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
 import NavBar from "../Navbar/Navbar";
-import classes from "./Cpp.module.css";
+import classes from "./Topic.module.css";
 import Topics from "../Topics/Topics";
 
-class Cpp extends Component {
+class Topic extends Component {
   state = {
     showTopics: false
   };
@@ -15,15 +15,18 @@ class Cpp extends Component {
   };
 
   render() {
+    const path = "/" + this.props.type;
     return (
       <div>
-        <NavBar title="C++" />
+        <NavBar title={this.props.type} />
         <div className={classes.container}>
           <div className={classes.tabs}>
             <h2 onClick={this.showTopics}>Learn</h2>
             <h2>Practice</h2>
           </div>
-          {this.state.showTopics ? <Topics topic="cpp" path="/cpp" /> : null}
+          {this.state.showTopics ? (
+            <Topics topic={this.props.type} path={path} />
+          ) : null}
         </div>
       </div>
     );
